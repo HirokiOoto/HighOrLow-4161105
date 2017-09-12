11f741dead3586d2208d8d69d1705a9b17848083
Hiroki Ooto <hiroki.oto.sys@gmail.com>
2017-09-12 10:57:49 +0900

    main文とメソッドの作成

---
+++ src/HighOrLow.java
@@ -0,0 +1,16 @@
+public class HighOrLow {
+
+	static int win = 0;
+	//メソッドの呼び出し実行
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("GAME_START");
+		int win = hantei();
+		System.out.println();
+	}
+	//ゲーム判定メソッド
+	public static int hantei(){
+		return win;
+	}
+
+}
